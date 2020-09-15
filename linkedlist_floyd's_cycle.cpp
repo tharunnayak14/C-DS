7a3361d1297997354f123d93cfc2eeb38707f13d
Badavath Tharun <52671445+tharunnayak14@users.noreply.github.com>
2020-09-15 11:32:28 +0530

    Add files via upload

---
+++ linkedlist_floyd's_cycle.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node *next;
+
+        node(int d){ // parametrised constructor
+            data = d;
+            next = NULL;
+        }
+};
+void insert(node*&head, int d){ // pass by ref so that copy is not created
+    if(head==NULL){
+        head = new node(d);
+        return;
+    }
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+void insertTail(node*&head, int d){
+    if(head==NULL){
+        head = new node(d);
+        return;
+    }
+    node *tail = head;
+    while(tail->next!=NULL){
+        tail = tail->next;
+    }
+    tail->next = new node(d);
+    return;
+}
+void print(node* head){
+    while(head!=NULL){
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "\n";
+}
+node* take_input(){
+    int d;
+    cin >> d;
+    node *head = NULL;
+    while(d!=-1){
+        insert(head, d);
+        //insertTail(head, d);
+        cin >> d;
+    }
+    return head;
+}
+node* midpoint(node* head){
+    if(head==NULL or head->next==NULL){
+        return head;
+    }
+    node *slow = head;
+    node *fast = head->next;
+    while(fast->next!=NULL and fast!=NULL){
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+bool detect_cycle(node * head){
+    node *s = head;
+    node *f = head;
+
+    while(fast->next!=NULL and fast!=NULL){
+        f = f->next->next;
+        s = s->next;
+        if(f==s){
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+    node *head = take_input();
+    print(head);
+    node* mid = midpoint(head);
+    cout << mid->data;
+
+}
