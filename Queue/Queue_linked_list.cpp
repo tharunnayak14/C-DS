940d15fe6a8802235c699b35bd0ce5c8f62651c2
Badavath Tharun <52671445+tharunnayak14@users.noreply.github.com>
2020-09-23 16:45:37 +0530

    Add files via upload

---
+++ Queue/Queue_linked_list.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+class node{
+public:
+    int data;
+    node *next;
+
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+class queue{
+public:
+    node *front, *rear;
+    queue(){
+        front = rear = NULL;
+    }
+    void push(int d){
+       node* temp = new node(d); 
+        if (rear == NULL) { 
+            front = rear = temp; 
+            return; 
+        } 
+        rear->next = temp; 
+        rear = temp; 
+    }
+    void pop(){
+        if (front == NULL) 
+            return; 
+        node* temp = front; 
+        front = front->next;
+
+        if (front == NULL) 
+            rear = NULL; 
+  
+        delete (temp); 
+    }
+};
+
+int main(){
+    queue q;
+    q.push(3);
+    q.push(88);
+    q.push(8);
+    cout << q.front->data<<"\n";
+    cout << q.rear->data;
+}
