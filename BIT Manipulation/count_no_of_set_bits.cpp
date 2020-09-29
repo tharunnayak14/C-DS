44d59ea85563d2ae8a67af9fe15b08869c83a576
Badavath Tharun <52671445+tharunnayak14@users.noreply.github.com>
2020-09-29 17:02:29 +0530

    Update count_no_of_set_bits.cpp

--- BIT Manipulation/count_no_of_set_bits.cpp
+++ BIT Manipulation/count_no_of_set_bits.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
-int count_set_bits(int n) {
+int slow_method(int n) {
+    // complexity O(no of bits)
+
+    // atmax there are log(n)+1 bits for a number n
     int ans = 0;
     while(n>0)
     {
@@ -9,11 +12,16 @@ int count_set_bits(int n) {
     }
     return ans;
 }
-int second_method(int n) {
+int efficient_method(int n) {
+    // complexity O(no of set bits)
     int ans = 0;
+    //no of times while loop runs is ans
     while(n>0)
     {
-        n = n & (n - 1);
+        n = n & (n - 1); // this line removes the last set bit (goes from right to left)
+        // let n   = 1001
+        // n-1     = 1000
+        // n&(n-1) = 1000
         ans++;
     }
     return ans;
@@ -21,8 +29,8 @@ int second_method(int n) {
 int main() {
     int n;
     cin >> n;
-    cout<<count_set_bits(n)<<"\n";
-    cout << second_method(n) << "\n";
-    cout << __builtin_popcount(n) << "\n";
+    cout << slow_method(n)<<"\n";
+    cout << efficient_method(n) << "\n";
+    cout << __builtin_popcount(n) << "\n"; // inbuilt function in cpp to find no of set bits
     return 0;
 }
