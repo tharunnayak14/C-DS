8fe981ec9420af85cde5a70682696ee928827513
Badavath Tharun <52671445+tharunnayak14@users.noreply.github.com>
2020-09-29 16:51:38 +0530

    Update replace_n_by_m.cpp

--- BIT Manipulation/replace_n_by_m.cpp
+++ BIT Manipulation/replace_n_by_m.cpp
@@ -6,6 +6,20 @@ int clear_range_i_to_j(int n, int i, int j) {
     return n & (a | b);
 }
 int replace(int n, int m, int i, int j) {
+    /*
+        let n = 1011011111
+        let m = 1101
+        i = 1
+        j = 4
+        clear all bits from range i to j gives
+        n = 1011000001
+        m = m<<i;
+        makes m = 11010
+
+        now simply take or of both n and m which gives
+
+        10110(1101)1 replaced
+    */
     int x = clear_range_i_to_j(n, i, j);
     return x | (m<<i);
 }
