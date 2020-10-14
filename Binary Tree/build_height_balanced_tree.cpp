71ad9a0bacf64fdc97ce1f7b7229b731bc31ba40
Badavath Tharun <52671445+tharunnayak14@users.noreply.github.com>
2020-10-14 17:57:30 +0530

    Add files via upload

---
+++ Binary Tree/build_height_balanced_tree.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node *left;
+    node *right;
+    // constructor for creating new node given some int data
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node * build_tree_from_array(int *a, int s,int e){
+    if(s>e){
+        return NULL;
+    }
+    int mid = (s + e) / 2;
+    node *root = new node(a[mid]);
+
+    root->left = build_tree_from_array(a, s, mid - 1);
+    root->right = build_tree_from_array(a, mid + 1, e);
+}
+void bfs(node *root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        node* f = q.front();
+        if(f==NULL){
+            cout<<endl;
+            q.pop();
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<f->data<<",";
+            q.pop();
+
+            if(f->left){
+                q.push(f->left);
+            }
+            if(f->right){
+                q.push(f->right);
+            }
+        }
+    }
+    return;
+}
+int main(){
+
+    int ar[] = {1, 2, 3, 4, 5,  6, 7,};
+
+    node *root = build_tree_from_array(ar, 0, 6);
+    bfs(root);
+
+    return 0;
+}
---
+++ Binary Tree/build_tree_from_preorder_and_inorder.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*left;
+        node*right;
+
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+void print(node *root){
+    if(root==NULL){
+        return;
+    }
+    //Otherwise, print root first followed by subtrees(children)
+    cout<<root->data<<" ";
+    print(root->left);
+    print(root->right);
+}
+
+void printIn(node*root){
+    if(root==NULL){
+        return;
+    }
+    //Otherwise Left Root Right
+    printIn(root->left);
+    cout<<root->data<<" ";
+    printIn(root->right);
+}
+
+void printPost(node*root){
+    if(root==NULL){
+        return;
+    }
+    printPost(root->left);
+    printPost(root->right);
+    cout<<root->data<<" ";
+}
+
+
+void bfs(node *root){
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        node* f = q.front();
+        if(f==NULL){
+            cout<<endl;
+            q.pop();
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<f->data<<",";
+            q.pop();
+
+            if(f->left){
+                q.push(f->left);
+            }
+            if(f->right){
+                q.push(f->right);
+            }
+        }
+    }
+    return;
+}
+
+
+
+node* buildTreeFromArray(int *a,int s,int e){
+    //Base Case
+    if(s>e){
+        return NULL;
+    }
+    //Recursive Case
+    int mid = (s+e)/2;
+    node* root = new node(a[mid]);
+    root->left = buildTreeFromArray(a,s,mid-1);
+    root->right = buildTreeFromArray(a,mid+1,e);
+    return root;
+
+}
+
+int main(){
+    // preorder 1 2 3 4 8 5 6 7 
+    // inorder  3 2 8 4 1 6 7 5
+    int pre[] = {1, 2, 3, 4, 8, 5, 6, 7};
+    int ino[] = {3, 2, 8, 4, 1, 6, 7, 5};
+
+    int n = sizeof(ino) / sizeof(pre);
+
+    node *root = create_tree(ino, pre, 0, n-1);
+    print_inorder(root);
+
+    return 0;
+}
