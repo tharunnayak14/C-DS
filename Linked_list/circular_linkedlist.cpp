dbfdaa448a481d2944cade9feaee7234fe51db07
Badavath Tharun <52671445+tharunnayak14@users.noreply.github.com>
2021-01-06 11:18:15 +0700

    Update circular_linkedlist.cpp

--- Linked_list/circular_linkedlist.cpp
+++ Linked_list/circular_linkedlist.cpp
@@ -27,10 +27,25 @@ void insert_at_head(node*&head, int data){
     }
     head = n;
 }
+void insert_at_tail(node*&head, int data){
+    if(head==NULL){
+        head = new node(data);
+        head->next = head;
+        return;
+    }
+    node *n = new node(data);
+    node*temp = head;
+    while(temp->next!=head){
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next = head;
+
+}
 void print(node*head){
     node *temp = head;
     while(temp->next!=head){
-        cout << temp->data << " ->";
+        cout << temp->data << " -> ";
         temp = temp->next;
     }
     cout << temp->data << "\n";
@@ -38,11 +53,21 @@ void print(node*head){
 }
 int main(){
     node *head = NULL;
+    insert_at_tail(head, 4);
+    print(head);
     insert_at_head(head, 5);
+    print(head);
     insert_at_head(head, 2);
+    print(head);
     insert_at_head(head, 3);
+    print(head);
     insert_at_head(head, 1);
+    print(head);
     insert_at_head(head, 8);
+    print(head);
+    insert_at_tail(head, 6);
+    print(head);
+    insert_at_tail(head, 90);
     print(head);
     return 0;
 }
