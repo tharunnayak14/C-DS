ab26ad1d62c35bb961b9583b4ae8531189163264
Badavath Tharun <52671445+tharunnayak14@users.noreply.github.com>
2021-01-08 13:16:49 +0700

    Update stack_reverse.cpp

--- Stack/stack_reverse.cpp
+++ Stack/stack_reverse.cpp
@@ -1,37 +1,49 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <vector>
+#include <stack>
 
 using namespace std;
-void transfer(stack<int>&s, stack<int>&p, int n){
-        for (int i = 0; i < n;i++){
-            p.push(s.top());
-            s.pop();
-        }
+void transfer(stack<int> &s, stack<int> &p, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        p.push(s.top());
+        s.pop();
+    }
 }
-void reverse(stack<int>&s){
+void reverse(stack<int> &s)
+{
     stack<int> p;
     int n = s.size();
-    for (int i = 0; i < n;i++){
+    for (int i = 0; i < n; i++)
+    {
         //store top
         int x = s.top();
         s.pop();
-        //transfer every element to new stack
-        transfer(s, p, n-1-i);
-        //push stored top into stack
+
+        //transfer every n-1-i elements to new stack
+        transfer(s, p, n - 1 - i);
+
+        //push stored top into the original stack
         s.push(x);
-        //transfer elements back into stack from new stack
-        transfer(p, s, n-i-1);
+        
+        //transfer elements back into the original stack from new stack
+        transfer(p, s, n - i - 1);
     }
 }
-int main(){
+int main()
+{
 
     stack<int> s;
-    for (int i = 1; i < 6;i++){
+    for (int i = 1; i < 6; i++)
+    {
         s.push(i * i);
     }
+    //O(N**2) time complexity
+    //O(N) space complexity
     reverse(s);
-    while(!s.empty()){
+    while (!s.empty())
+    {
         cout << s.top() << " ";
         s.pop();
     }
